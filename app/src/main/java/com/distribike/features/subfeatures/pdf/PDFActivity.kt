432e3947ab84ae05559a54dc4f7b53e3e12aa7f4
Shayds34 <shayds@hotmail.fr>
2023-03-06 00:51:54 +0100

    feat: add transmission data form to PDF

--- app/src/main/java/com/distribike/features/subfeatures/pdf/PDFActivity.kt
+++ app/src/main/java/com/distribike/features/subfeatures/pdf/PDFActivity.kt
@@ -314,9 +314,9 @@ class PDFActivity : ComponentActivity() {
         canvas1.drawText(concessionName, 831F, 227F, textPaint)
         canvas1.drawText(concessionCode, 831F, 267F, textPaint)
         canvas1.drawText(position, 762F, 312F, textPaint)
-       canvas1.drawText("$articulation cm", 370F, 1393F, textPaint)
-        canvas1.drawText(markLeft, 750F, 1462F, textPaint)
-        canvas1.drawText(markRight, 900F, 1462F, textPaint)
+        canvas1.drawText(sections.transmissionSteps?.stepEntityModels?.get(1)?.additionalInfo ?: "", 370F, 1393F, textPaint)
+        canvas1.drawText(sections.transmissionSteps?.stepEntityModels?.get(3)?.additionalInfo ?: "", 750F, 1462F, textPaint)
+        canvas1.drawText(sections.transmissionSteps?.stepEntityModels?.get(3)?.additionalInfo2 ?: "", 900F, 1462F, textPaint)
 
         // GLOBAL POSITION
         // Normalement ces positions ne devraient jamais changer,
@@ -713,7 +713,7 @@ class PDFActivity : ComponentActivity() {
         //)
 
         // To test on EMULATOR
-        val file = File(Environment.getExternalStorageDirectory(), "GFG.pdf")
+        val file = File(Environment.getExternalStorageDirectory(), "$chassis.pdf")
 
 
         try {
