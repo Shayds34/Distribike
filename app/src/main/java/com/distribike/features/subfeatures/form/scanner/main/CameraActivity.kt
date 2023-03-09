d249d9112e45c90d8823e41bf154bd7d5d34098b
Shayds <the.shades34@gmail.com>
2023-03-10 00:25:00 +0100

    feat: test on camera Preview

--- app/src/main/java/com/distribike/features/subfeatures/form/scanner/main/CameraActivity.kt
+++ app/src/main/java/com/distribike/features/subfeatures/form/scanner/main/CameraActivity.kt
@@ -16,21 +16,13 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.twotone.Close
-import com.distribike.ui.theme.CutOutShape
-import androidx.compose.material.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -90,103 +82,62 @@ class CameraActivity : ComponentActivity(), LifecycleOwner {
             modifier = Modifier.fillMaxWidth(),
             contentAlignment = Alignment.Center
         ) {
-            AndroidView(factory = { AndroidViewContext ->
-                PreviewView(AndroidViewContext).apply {
-                    this.scaleType = PreviewView.ScaleType.FILL_CENTER
-                    layoutParams = ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                    )
-                    implementationMode = PreviewView.ImplementationMode.COMPATIBLE
-                }
-            }, modifier = Modifier.size(600.dp, 200.dp).align(Alignment.Center), update = { previewView ->
-                val cameraSelector: CameraSelector =
-                    CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                        .build()
-                val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
-                val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
-                    ProcessCameraProvider.getInstance(context)
-
-                cameraProviderFuture.addListener({
-                    preview = Preview.Builder().build().also {
-                        it.setSurfaceProvider(previewView.surfaceProvider)
+            AndroidView(
+                factory = { AndroidViewContext ->
+                    PreviewView(AndroidViewContext).apply {
+                        this.scaleType = PreviewView.ScaleType.FILL_CENTER
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                        )
+                        implementationMode = PreviewView.ImplementationMode.COMPATIBLE
                     }
+                },
+                modifier = Modifier.size(600.dp, 200.dp),
+                update = { previewView ->
+                    val cameraSelector: CameraSelector =
+                        CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                            .build()
+                    val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+                    val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+                        ProcessCameraProvider.getInstance(context)
 
-                    val screenWidth = applicationContext.resources.displayMetrics.widthPixels
-                    val screenHeight = applicationContext.resources.displayMetrics.heightPixels
+                    cameraProviderFuture.addListener({
+                        preview = Preview.Builder().build().also {
+                            it.setSurfaceProvider(previewView.surfaceProvider)
+                        }
 
-                    val width = screenHeight / 2
-                    val height = screenWidth / 2
+                        val screenWidth = applicationContext.resources.displayMetrics.widthPixels
+                        val screenHeight = applicationContext.resources.displayMetrics.heightPixels
 
-                    val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-                    val barcodeAnalyser = BarcodeAnalyser { barcodes ->
-                        barcodes.forEach { barcode ->
-                            barcode.rawValue?.let { barcodeValue ->
-                                viewModel.showDialog(barcodeValue)
+                        val width = screenHeight / 2
+                        val height = screenWidth / 2
+
+                        val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+                        val barcodeAnalyser = BarcodeAnalyser { barcodes ->
+                            barcodes.forEach { barcode ->
+                                barcode.rawValue?.let { barcodeValue ->
+                                    viewModel.showDialog(barcodeValue)
+                                }
                             }
                         }
-                    }
 
-                    val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                        .setTargetResolution(android.util.Size(600, 200)).build().also {
-                            it.setAnalyzer(cameraExecutor, barcodeAnalyser)
+                        val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
+                            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                            .setTargetResolution(android.util.Size(600, 200)).build().also {
+                                it.setAnalyzer(cameraExecutor, barcodeAnalyser)
+                            }
+
+                        try {
+                            cameraProvider.unbindAll()
+                            cameraProvider.bindToLifecycle(
+                                lifecycleOwner, cameraSelector, preview, imageAnalysis
+                            )
+                        } catch (e: Exception) {
+                            Log.d("TAG", "CameraPreview: ${e.localizedMessage}")
                         }
-
-                    try {
-                        cameraProvider.unbindAll()
-                        cameraProvider.bindToLifecycle(
-                            lifecycleOwner, cameraSelector, preview, imageAnalysis
-                        )
-                    } catch (e: Exception) {
-                        Log.d("TAG", "CameraPreview: ${e.localizedMessage}")
-                    }
-                }, ContextCompat.getMainExecutor(context))
-            })
-            // Surface(
-            //     shape = CutOutShape(),
-            //     color = Color.Black.copy(alpha = 0.55f),
-            //     modifier = Modifier
-            //         .fillMaxSize()
-            // ) {
-//
-            // }
-
-            Column {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    IconButton(onClick = {
-                        finish()
-                    }) {
-                        Icon(
-                            imageVector = Icons.TwoTone.Close,
-                            contentDescription = null,
-                            tint = Color.White
-                        )
-                    }
-
-                    Text(
-                        text = "Scanner de code à barres", color = Color.White, fontSize = 20.sp
-                    )
-                }
-
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 24.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Text(
-                        text = "Placer le code à barre à scanner à l'intérieur du rectangle.",
-                        color = Color.White,
-                        fontSize = 12.sp
-                    )
-                }
-            }
+                    }, ContextCompat.getMainExecutor(context))
+                })
         }
-
     }
-
 }
